eafcde605e0de112c2bca43167fb56309b8316a2
juancj1 <118417999+juancj1@users.noreply.github.com>
2023-08-02 20:00:49 +0700

    Add files via upload

---
+++ imgpros/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,220 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        MenuStrip1 = New MenuStrip()
+        ToolStripMenuItem1 = New ToolStripMenuItem()
+        BukaFileToolStripMenuItem = New ToolStripMenuItem()
+        ToolStripMenuItem2 = New ToolStripSeparator()
+        KeluarToolStripMenuItem = New ToolStripMenuItem()
+        ProssesToolStripMenuItem = New ToolStripMenuItem()
+        KonfersiWarnaToolStripMenuItem = New ToolStripMenuItem()
+        BlueToolStripMenuItem = New ToolStripMenuItem()
+        GrayToolStripMenuItem = New ToolStripMenuItem()
+        GreenToolStripMenuItem = New ToolStripMenuItem()
+        NegativeToolStripMenuItem = New ToolStripMenuItem()
+        RedToolStripMenuItem = New ToolStripMenuItem()
+        ToolStripMenuItem3 = New ToolStripSeparator()
+        PenajamanToolStripMenuItem = New ToolStripMenuItem()
+        PenajamanToolStripMenuItem1 = New ToolStripMenuItem()
+        PictureBox1 = New PictureBox()
+        StatusStrip1 = New StatusStrip()
+        ToolStripStatusLabel1 = New ToolStripStatusLabel()
+        PictureBox2 = New PictureBox()
+        MenuStrip1.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        StatusStrip1.SuspendLayout()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.ImageScalingSize = New Size(24, 24)
+        MenuStrip1.Items.AddRange(New ToolStripItem() {ToolStripMenuItem1, ProssesToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(1455, 33)
+        MenuStrip1.TabIndex = 2
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' ToolStripMenuItem1
+        ' 
+        ToolStripMenuItem1.DropDownItems.AddRange(New ToolStripItem() {BukaFileToolStripMenuItem, ToolStripMenuItem2, KeluarToolStripMenuItem})
+        ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        ToolStripMenuItem1.Size = New Size(54, 29)
+        ToolStripMenuItem1.Text = "File"
+        ' 
+        ' BukaFileToolStripMenuItem
+        ' 
+        BukaFileToolStripMenuItem.Name = "BukaFileToolStripMenuItem"
+        BukaFileToolStripMenuItem.Size = New Size(270, 34)
+        BukaFileToolStripMenuItem.Text = "Buka File"
+        ' 
+        ' ToolStripMenuItem2
+        ' 
+        ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        ToolStripMenuItem2.Size = New Size(267, 6)
+        ' 
+        ' KeluarToolStripMenuItem
+        ' 
+        KeluarToolStripMenuItem.Name = "KeluarToolStripMenuItem"
+        KeluarToolStripMenuItem.Size = New Size(270, 34)
+        KeluarToolStripMenuItem.Text = "Keluar"
+        ' 
+        ' ProssesToolStripMenuItem
+        ' 
+        ProssesToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {KonfersiWarnaToolStripMenuItem, ToolStripMenuItem3, PenajamanToolStripMenuItem})
+        ProssesToolStripMenuItem.Name = "ProssesToolStripMenuItem"
+        ProssesToolStripMenuItem.Size = New Size(88, 29)
+        ProssesToolStripMenuItem.Text = "Prosses"
+        ' 
+        ' KonfersiWarnaToolStripMenuItem
+        ' 
+        KonfersiWarnaToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {BlueToolStripMenuItem, GrayToolStripMenuItem, GreenToolStripMenuItem, NegativeToolStripMenuItem, RedToolStripMenuItem})
+        KonfersiWarnaToolStripMenuItem.Name = "KonfersiWarnaToolStripMenuItem"
+        KonfersiWarnaToolStripMenuItem.Size = New Size(270, 34)
+        KonfersiWarnaToolStripMenuItem.Text = "Citra Warna"
+        ' 
+        ' BlueToolStripMenuItem
+        ' 
+        BlueToolStripMenuItem.Name = "BlueToolStripMenuItem"
+        BlueToolStripMenuItem.Size = New Size(184, 34)
+        BlueToolStripMenuItem.Text = "fusion"
+        ' 
+        ' GrayToolStripMenuItem
+        ' 
+        GrayToolStripMenuItem.Name = "GrayToolStripMenuItem"
+        GrayToolStripMenuItem.Size = New Size(184, 34)
+        GrayToolStripMenuItem.Text = "Gray"
+        ' 
+        ' GreenToolStripMenuItem
+        ' 
+        GreenToolStripMenuItem.Name = "GreenToolStripMenuItem"
+        GreenToolStripMenuItem.Size = New Size(184, 34)
+        GreenToolStripMenuItem.Text = "Green"
+        ' 
+        ' NegativeToolStripMenuItem
+        ' 
+        NegativeToolStripMenuItem.Name = "NegativeToolStripMenuItem"
+        NegativeToolStripMenuItem.Size = New Size(184, 34)
+        NegativeToolStripMenuItem.Text = "Negative"
+        ' 
+        ' RedToolStripMenuItem
+        ' 
+        RedToolStripMenuItem.Name = "RedToolStripMenuItem"
+        RedToolStripMenuItem.Size = New Size(184, 34)
+        RedToolStripMenuItem.Text = "Red"
+        ' 
+        ' ToolStripMenuItem3
+        ' 
+        ToolStripMenuItem3.Name = "ToolStripMenuItem3"
+        ToolStripMenuItem3.Size = New Size(267, 6)
+        ' 
+        ' PenajamanToolStripMenuItem
+        ' 
+        PenajamanToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {PenajamanToolStripMenuItem1})
+        PenajamanToolStripMenuItem.Name = "PenajamanToolStripMenuItem"
+        PenajamanToolStripMenuItem.Size = New Size(270, 34)
+        PenajamanToolStripMenuItem.Text = "Thresold"
+        ' 
+        ' PenajamanToolStripMenuItem1
+        ' 
+        PenajamanToolStripMenuItem1.Name = "PenajamanToolStripMenuItem1"
+        PenajamanToolStripMenuItem1.Size = New Size(270, 34)
+        PenajamanToolStripMenuItem1.Text = "Penajaman"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources._424f66998934ce0a53309d59a4a77368
+        PictureBox1.Location = New Point(12, 51)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(675, 510)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' StatusStrip1
+        ' 
+        StatusStrip1.ImageScalingSize = New Size(24, 24)
+        StatusStrip1.Items.AddRange(New ToolStripItem() {ToolStripStatusLabel1})
+        StatusStrip1.Location = New Point(0, 621)
+        StatusStrip1.Name = "StatusStrip1"
+        StatusStrip1.Size = New Size(1455, 32)
+        StatusStrip1.TabIndex = 3
+        StatusStrip1.Text = "StatusStrip1"
+        ' 
+        ' ToolStripStatusLabel1
+        ' 
+        ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        ToolStripStatusLabel1.Size = New Size(392, 25)
+        ToolStripStatusLabel1.Text = "JUAN CERAH JOSEPH TI20E (20200040076)"
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = My.Resources.Resources._424f66998934ce0a53309d59a4a77368
+        PictureBox2.Location = New Point(752, 51)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(675, 510)
+        PictureBox2.TabIndex = 4
+        PictureBox2.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1455, 653)
+        Controls.Add(PictureBox2)
+        Controls.Add(StatusStrip1)
+        Controls.Add(PictureBox1)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "Form1"
+        Text = "Pengolahan Citra"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        StatusStrip1.ResumeLayout(False)
+        StatusStrip1.PerformLayout()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents BukaFileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripSeparator
+    Friend WithEvents KeluarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProssesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents KonfersiWarnaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BlueToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GrayToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GreenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NegativeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RedToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem3 As ToolStripSeparator
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PenajamanToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PenajamanToolStripMenuItem1 As ToolStripMenuItem
+End Class
---
+++ imgpros/WinFormsApp1/Form1.vb
@@ -0,0 +1,185 @@
+﻿
+Imports System.Drawing.Drawing2D
+Public Class Form1
+    Inherits System.Windows.Forms.Form
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ToolStripStatusLabel1_Click(sender As Object, e As EventArgs) Handles ToolStripStatusLabel1.Click
+
+    End Sub
+
+    Private Sub BukaFileToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BukaFileToolStripMenuItem.Click
+        Dim BukaFile As New OpenFileDialog
+
+        Try
+            BukaFile.Title = "Buka File Gambar"
+            BukaFile.DefaultExt = "bmp"
+            BukaFile.Filter = "File Citra|*.bmp;*.jpg;*.png"
+
+            If BukaFile.ShowDialog = Windows.Forms.DialogResult.OK Then
+                PictureBox1.Image = New Bitmap(BukaFile.FileName)
+                PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+
+            End If
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub KeluarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KeluarToolStripMenuItem.Click
+        Try
+            Dispose()
+
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub BlueToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BlueToolStripMenuItem.Click
+        Dim r, g, b As Integer
+        Dim picSource As New Bitmap(PictureBox1.Image)
+        Dim picDest As New Bitmap(picSource.Width, picSource.Height)
+
+        For i = 0 To picSource.Width - 1
+            For j = 0 To picSource.Height - 1
+                r = picSource.GetPixel(i, j).R
+                g = picSource.GetPixel(i, j).G
+                b = picSource.GetPixel(i, j).B
+
+                If i < picSource.Width / 6 Then
+                    picDest.SetPixel(i, j, Color.FromArgb(0, g, 0)) ' Hijau
+                ElseIf i < 2 * picSource.Width / 6 Then
+                    picDest.SetPixel(i, j, Color.FromArgb(r, 0, 0)) ' Merah
+                ElseIf i < 3 * picSource.Width / 6 Then
+                    picDest.SetPixel(i, j, Color.FromArgb(r, g, 0)) ' Kuning
+                ElseIf i < 4 * picSource.Width / 6 Then
+                    picDest.SetPixel(i, j, Color.FromArgb(0, 0, b)) ' Biru
+                ElseIf i < 5 * picSource.Width / 6 Then
+                    picDest.SetPixel(i, j, Color.FromArgb(r, 0, b)) ' Ungu
+                Else
+                    picDest.SetPixel(i, j, Color.FromArgb(r, 0, b)) ' Ungu
+                End If
+            Next
+        Next
+
+        PictureBox2.Image = picDest
+    End Sub
+
+    Private Sub GreenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GreenToolStripMenuItem.Click
+        Dim r, g, b As Integer
+        Dim picSource As New Bitmap(PictureBox1.Image)
+        Dim picDest = picSource
+        PictureBox2.Image = picDest
+
+        For i = 1 To picSource.Width - 1
+            For j = 1 To picSource.Height - 1
+                r = picSource.GetPixel(i, j).R
+                g = picSource.GetPixel(i, j).G
+                b = picSource.GetPixel(i, j).B
+
+                picDest.SetPixel(i, j, Color.FromArgb(0, g, 0))
+
+            Next
+        Next
+    End Sub
+
+    Private Sub NegativeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NegativeToolStripMenuItem.Click
+        Dim r, g, b As Integer
+        Dim picSource As New Bitmap(PictureBox1.Image)
+        Dim picDest = picSource
+        PictureBox2.Image = picDest
+
+        For i = 1 To picSource.Width - 1
+            For j = 1 To picSource.Height - 1
+                r = picSource.GetPixel(i, j).R
+                g = picSource.GetPixel(i, j).G
+                b = picSource.GetPixel(i, j).B
+
+                r = 255 - r
+                g = 255 - g
+                b = 255 - b
+                picDest.SetPixel(i, j, Color.FromArgb(r, g, b))
+            Next
+        Next
+    End Sub
+
+    Private Sub ThresoldToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NegativeToolStripMenuItem.Click
+        Dim r, g, b As Integer
+        Dim picSource As New Bitmap(PictureBox1.Image)
+        Dim picDest = picSource
+        PictureBox2.Image = picDest
+
+        Dim thresh As Integer
+        thresh = 100
+
+        Dim gray As Integer
+        For i = 1 To picSource.Width - 1
+            For j = 1 To picSource.Height - 1
+                r = picSource.GetPixel(i, j).R
+                g = picSource.GetPixel(i, j).G
+                b = picSource.GetPixel(i, j).B
+
+                gray = CInt((r + g + b) / 3)
+
+                If gray >= thresh Then
+                    r = 255
+                    g = 255
+                    b = 255
+                Else
+                    r = 0
+                    g = 0
+                    b = 0
+                End If
+                picDest.SetPixel(i, j, Color.FromArgb(r, g, b))
+
+            Next
+        Next
+    End Sub
+
+
+    Private Sub PenajamanToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PenajamanToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub GrayToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GrayToolStripMenuItem.Click
+        Dim r, g, b As Integer
+        Dim picSource As New Bitmap(PictureBox1.Image)
+        Dim picDest = picSource
+        PictureBox2.Image = picDest
+
+        Dim gray As Integer
+        For i = 1 To picSource.Width - 1
+            For j = 1 To picSource.Height - 1
+                r = picSource.GetPixel(i, j).R
+                g = picSource.GetPixel(i, j).G
+                b = picSource.GetPixel(i, j).B
+
+                gray = CInt((r + g + b) / 3)
+
+                picDest.SetPixel(i, j, Color.FromArgb(gray, gray, gray))
+
+            Next
+        Next
+
+    End Sub
+
+    Private Sub RedToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RedToolStripMenuItem.Click
+        Dim r, g, b As Integer
+        Dim picSource As New Bitmap(PictureBox1.Image)
+        Dim picDest = picSource
+        PictureBox2.Image = picDest
+
+        For i = 1 To picSource.Width - 1
+            For j = 1 To picSource.Height - 1
+                r = picSource.GetPixel(i, j).R
+                g = picSource.GetPixel(i, j).G
+                b = picSource.GetPixel(i, j).B
+
+                picDest.SetPixel(i, j, Color.FromArgb(r, 0, 0))
+
+            Next
+        Next
+    End Sub
+End Class
+
